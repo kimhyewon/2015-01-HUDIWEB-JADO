5925f13d65f9b4f8039a07caa55a7d29b3627449
Smith <smithoo@nhnnext.org>
2015-03-19 21:19:17 +0900

    Sign up basic

--- src/jado/controller/SignUpController.java
+++ src/jado/controller/SignUpController.java
@@ -1,32 +1,41 @@
 package jado.controller;
 
 import jado.model.NormalUser;
+import jado.model.Seller;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/user")
 public class SignUpController extends HttpServlet {
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+
+		//Normal User
 		String userId = req.getParameter("userId");
 		String password = req.getParameter("password");
 		String name = req.getParameter("name");
 		String phone = req.getParameter("phone");
 		String address = req.getParameter("address");
-		
-		if (req.getParameter("a") != null) {
-			
+		NormalUser user = null;
+
+		if (req.getParameter("isSeller") != null) {
+			String shopUrl = req.getParameter("shopUrl");
+			String shopPhone = req.getParameter("shopPhone");
+			String bank = req.getParameter("bank");
+			String bankAccount = req.getParameter("bankAccount");
+			user = new Seller(userId, password, name, phone, address, shopUrl, shopPhone, bank, bankAccount);
 		} else {
-			
+			user = new NormalUser(userId, password, name, phone, address);
 		}
-		NormalUser user = new NormalUser(userId, password, name, phone, address);
 		
 		resp.sendRedirect("/");
-		
 	}
 }
