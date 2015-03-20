0e3a77d5ce95e40dc60b0c0c0c3acc7ead8f33e3
NEXT <NEXT@192.168.1.129>
2015-03-20 17:31:34 +0900

    #5 회원가입 사이클 구현 완료

--- test/jado/dao/UserDaoTest.java
+++ test/jado/dao/UserDaoTest.java
@@ -1,15 +1,21 @@
 package jado.dao;
 
 import jado.model.Customer;
+import jado.model.Seller;
 
 import org.junit.Test;
 
 public class UserDaoTest {
 
 	@Test
-	public void test() {
-		Customer user = new Customer("erin314", "1111", "이경륜" , "01027723883", "경기도 안양시 동안구 비산1동" );
-		UserDao dao = new UserDao();
-		dao.insert(user);
+	public void insertCustomer() {
+		Customer user = new Customer("erin3141", "1111", "이경륜" , "01027723883", "경기도 안양시 동안구 비산1동" );
+		UserDao.insert(user);
+	}
+
+	@Test
+	public void insertSeller() {
+		Seller user = new Seller("erin3141", "erin", "01027723883", "신한은행", "1111111111" );
+		UserDao.insert(user);
 	}
 }
