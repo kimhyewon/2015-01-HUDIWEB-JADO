5dbc435670f738b6fe1ca2c4749c849ecaca9d38
dragonist <erin314@naver.com>
2015-03-24 18:42:08 +0900

    #17 회원가입 시 url 저장 구현 ver0.1

--- webapps/javascripts/submit.js
+++ webapps/javascripts/submit.js
@@ -1,5 +1,7 @@
 var url_list= document.querySelectorAll('input[name=url]');
 for (var j = 0; j < url_list.length; j++) {
 	console.log(url_list[j].value)
-    url_list[j].value = window.location.pathname;
+	if (url_list[j].value==="") {
+    	url_list[j].value = window.location.pathname;
+	};
 }
