5238c04b7e2b6a0fd9865fce2cf9f46fbba3d947
GraceLilac <lovelove8926@naver.com>
2015-03-24 16:39:26 +0900

    회원 탈퇴 기능 추가

--- src/jado/dao/UserDao.java
+++ src/jado/dao/UserDao.java
@@ -1,5 +1,10 @@
 package jado.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import net.slipp.user.User;
 import jado.model.Customer;
 import jado.model.Seller;
 import core.jdbc.JdbcTemplate;
@@ -54,5 +59,17 @@ public class UserDao {
 		String sql = "select count(SELLER_ID) AS count from SELLER";
 		RowMapper<Integer> rm = rs -> rs.getInt("count");
 		return jdbcTemplate.executeQuery(sql, rm);
+	}
+	
+	public void removeUser(Seller seller) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();	
+		String sql = "delete from USERS where userId = ?";
+		jdbcTemplate.executeUpdate(sql, seller.getUserId());		
+	}
+	
+	public void removeUser(Customer customer) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();	
+		String sql = "delete from USERS where userId = ?";
+		jdbcTemplate.executeUpdate(sql, customer.getUserId());		
 	}
 }
