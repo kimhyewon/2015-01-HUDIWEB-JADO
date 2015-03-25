633929b77611c4698d869e49c5fe0cbc6543135d
ho <hataeho1@naver.com>
2015-03-26 02:10:23 +0900

    #51 배포 스크립트 실행시 DB init

---
+++ resources/sql/insertTestSet.sql
@@ -0,0 +1,7 @@
+-- Test Set
+
+-- -----------------------------------------------------
+-- Table `jado_dev`.`USER`
+-- -----------------------------------------------------
+
+insert into USER VALUES('testuser@testuser.com', 'testuserPassword','name','phone','address','2015-03-26 12:34:56','2015-03-26 12:34:56','F');
