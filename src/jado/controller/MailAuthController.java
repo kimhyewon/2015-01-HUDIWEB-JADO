c9100181d587ad2eb5ea3560694697b2e543dcd2
ho <hataeho1@naver.com>
2015-03-30 05:04:52 +0900

    Add "Invalid  access" phase

--- src/jado/controller/MailAuthController.java
+++ src/jado/controller/MailAuthController.java
@@ -2,30 +2,32 @@ package jado.controller;
 
 import jado.dao.MailAuthDao;
 import jado.dao.UserDao;
-import jado.model.Customer;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import core.exception.PasswordMismatchException;
-import core.exception.UserNotFoundException;
 import core.mail.Mail;
 import core.mail.MailSender;
 import core.util.ServletRequestUtils;
 
 @WebServlet("/user/mailAuth")
 public class MailAuthController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	private static final Logger logger = LoggerFactory.getLogger(MailAuthController.class);
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//TODO 공통 에러 페이지가 만들어 지면 리다이렉트 시키기
+		resp.getWriter().print("<h1>Invalid  access</h1>");
+	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
