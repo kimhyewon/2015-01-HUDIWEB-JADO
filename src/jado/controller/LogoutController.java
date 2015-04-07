240e184b8197f047f21074cae61ad252e96a7974
ho <hataeho1@naver.com>
2015-04-07 20:54:55 +0900

    Controller convert and BugFix

--- src/jado/controller/LogoutController.java
+++ src/jado/controller/LogoutController.java
@@ -1,10 +1,6 @@
 package jado.controller;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LogoutController {
 
 	@RequestMapping(value = "/user/logout", method = RequestMethod.GET)
-	public String logOut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getSession().invalidate();;
+	public String logOut(HttpSession session) {
+		session.invalidate();
 		return "main";
 	}
 }
--- src/jado/dao/MailAuthDao.java
+++ src/jado/dao/MailAuthDao.java
@@ -2,7 +2,6 @@ package jado.dao;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
