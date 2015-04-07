9e6ef34ad8b1ba391fdf6611fc5c0b2715ab56f5
erin <erin314@naver.com>
2015-04-07 13:42:06 +0900

    #Spring bean property row mapper 적용

--- src/jado/dao/ArticleCommentDao.java
+++ src/jado/dao/ArticleCommentDao.java
@@ -2,13 +2,12 @@ package jado.dao;
 
 import jado.model.ArticleComment;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -24,30 +23,22 @@ public class ArticleCommentDao extends JdbcDaoSupport {
 		populator.addScripts(new ClassPathResource("sql/initDbSchema.sql"), new ClassPathResource("sql/insertTestSet.sql"));
 		DatabasePopulatorUtils.execute(populator, getDataSource());
 	}
-
-	public ArticleComment findById(final String userId) {
-		String sql = "select * from ARTICLE_COMMENT where USER_ID=?";
-		RowMapper<ArticleComment> rm = new RowMapper<ArticleComment>() {
-			@Override
-			public ArticleComment mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new ArticleComment(rs.getString(1), rs.getString(2), rs.getString(3), 
-						rs.getString(4), rs.getDate(5), rs.getString(6));
-			}
-		};		
-		return getJdbcTemplate().queryForObject(sql, rm, userId);
-	}
-	
 	
 	public int insert(final ArticleComment ac){
 		String sql = "insert into ARTICLE_COMMENT values(?, ?, ?, ?, null, ?)";
-		return getJdbcTemplate().update(sql, ac.getShopUrl(), ac.getArticleTitle(), ac.getBoardName(), ac.getUserId(), ac.getContent());
-	} 
+		Object[] args = new Object[]{ac.getShopUrl(), ac.getArticleTitle(), ac.getBoardName(), ac.getUserId(), ac.getContent()};
+		return getJdbcTemplate().update(sql, args);
+	}
+
+	public ArticleComment findByPk(final ArticleComment ac) {
+		String sql = "select * from ARTICLE_COMMENT where SHOP_URL=? and ARTICLE_TITLE=? and BOARD_NAME=? and USER_ID=? and COMMENT_TIME=?";
+		Object[] args = new Object[]{ac.getShopUrl(), ac.getArticleTitle(), ac.getBoardName(), ac.getUserId(), ac.getCommentTime()};
+		return (ArticleComment) getJdbcTemplate().query(sql, args, new BeanPropertyRowMapper<ArticleComment>(ArticleComment.class));
+	}
 	
-//	// select all
-//	public List<ArticleComment> selectAll(ArticleComment articleComment) {
-//		JdbcTemplate222 jdbcTemplate = new JdbcTemplate222();
-//		String sql = "select * from ARTICLE_COMMENT where SHOP_URL=? and BOARD_NAME=? and ARTICLE_TITLE=? and USER_ID=?";
-//		RowMapper<List> rm = rs -> new ArrayList<ArticleComment>();
-//		return jdbcTemplate.executeQuery(sql, rm, articleComment.getShopUrl(), articleComment.getBoardName(), articleComment.getArticleTitle(), articleComment.getUserId());
-//	}
+	public List<ArticleComment> findByArticle(final ArticleComment ac) {
+		String sql = "select * from ARTICLE_COMMENT where SHOP_URL=? and ARTICLE_TITLE=? and BOARD_NAME=?" ;
+		Object[] args = new Object[]{ac.getShopUrl(), ac.getArticleTitle(), ac.getBoardName()};
+		return getJdbcTemplate().query(sql, args, new BeanPropertyRowMapper<ArticleComment>(ArticleComment.class));
+	}
 }
--- test/jado/dao/ArticleCommentDaoTest.java
+++ test/jado/dao/ArticleCommentDaoTest.java
@@ -1,5 +1,9 @@
 package jado.dao;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import jado.model.ArticleComment;
 
 import org.junit.Test;
@@ -20,13 +24,28 @@ public class ArticleCommentDaoTest {
 
 	@Test
 	public void insert() {
-		ArticleComment articleComment = new ArticleComment("testUrl", "오픈 기념 이벤트", "공지사항", "erin314@naver.com", "감사합니다2222");
+		ArticleComment articleComment = new ArticleComment("testUrl", "오픈 기념 이벤트", "공지사항", "erin3142@naver.com", "감사합니다2222");
 		articleCommentDao.insert(articleComment);
 		log.debug("articleComment1 : {}", articleComment);
 
-		ArticleComment articleComment2 = articleCommentDao.findById(articleComment.getUserId());
-
+		List<ArticleComment> articleComment2 = articleCommentDao.findByArticle(articleComment);
 		log.debug("articleComment2 : {}", articleComment2);
+	}
+	
+	@Test
+	public void selectAll() throws Exception {
+		ArticleComment articleComment = new ArticleComment("testUrl", "오픈 기념 이벤트", "공지사항", "erin314@naver.com", "감사합니다2222");
+		articleCommentDao.insert(articleComment);
+		
+		List<ArticleComment> comments = articleCommentDao.findByArticle(articleComment);
+		System.out.println("comments");
+		System.out.println(comments.size());
+		System.out.println(comments.get(0));
+		ArticleComment comment = articleCommentDao.findByPk(comments.get(0));
+		
+		System.out.println("comments");
+		System.out.println(comment);
+		
 	}
 
 }
