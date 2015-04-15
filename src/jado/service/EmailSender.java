35407928b5882a4c469f169157b62e7b0ef332fb
erin <erin314@naver.com>
2015-04-15 19:06:20 +0900

    [Spring] mail - 비동기 완료

--- src/jado/service/EmailSender.java
+++ src/jado/service/EmailSender.java
@@ -7,7 +7,6 @@ import javax.mail.internet.MimeMessage.RecipientType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import core.mail.Mail;
@@ -17,7 +16,6 @@ public class EmailSender {
 	@Autowired
 	private JavaMailSender mailSender;
 
-	@Async
 	public void sendEmail(Mail mail) throws MessagingException {
 		MimeMessage msg = mailSender.createMimeMessage();
 
