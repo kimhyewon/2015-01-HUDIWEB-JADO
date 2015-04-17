f62bd3ae3c510feeddd9ed2c5229a46081acbb50
GraceLilac <lovelove8926@naver.com>
2015-04-17 19:18:07 +0900

    d

--- src/jado/controller/BlogController.java
+++ src/jado/controller/BlogController.java
@@ -1,21 +1,28 @@
 package jado.controller;
 
+import jado.model.Shop;
+import jado.service.BlogService;
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-public class BlogDummyController {
+public class BlogController {
+	@Autowired private BlogService blogService;
 	
-	@RequestMapping(value = "/blog", method = RequestMethod.GET)
+	@RequestMapping(value = "/{shopUrl}", method = RequestMethod.GET)
 	public String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Shop shop = new Shop();
+		blogService.insertShop(shop);
 		return "blogDummy";
 	}
-
 }
