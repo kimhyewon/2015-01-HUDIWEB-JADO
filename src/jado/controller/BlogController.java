7a3c03895c41aa808e5c4bafda9cca10e7162811
erin <erin314@naver.com>
2015-04-17 20:44:12 +0900

    [Blog] shop blog 완료

--- src/jado/controller/BlogController.java
+++ src/jado/controller/BlogController.java
@@ -1,28 +1,42 @@
 package jado.controller;
 
+import jado.dao.ShopDao;
 import jado.model.Shop;
-import jado.service.BlogService;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class BlogController {
-	@Autowired private BlogService blogService;
+	@Autowired private ShopDao shopDao;
 	
-	@RequestMapping(value = "/{shopUrl}", method = RequestMethod.GET)
-	public String doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Shop shop = new Shop();
-		blogService.insertShop(shop);
+	@RequestMapping(value = "/shop/{shopUrl}", method = RequestMethod.GET)
+	public String showShop(@PathVariable("shopUrl")String url, Model model) throws ServletException, IOException {
+		Shop shop = shopDao.selectByUrl(url);
+		if (shop == null) {
+			return "main";
+		}
+		model.addAttribute("shop", shop);
 		return "blogDummy";
 	}
+	
+	@RequestMapping(value = "/settingShop/{shopUrl}", method = RequestMethod.GET)
+	public String doGet(@PathVariable("shopUrl")String url, Model model) throws ServletException, IOException {
+		Shop shop = shopDao.selectByUrl(url);
+		if (shop == null) {
+			return "settingShop";
+		}
+		model.addAttribute("shop", shop);
+		return "blogDummy";
+	}
+	
 }
