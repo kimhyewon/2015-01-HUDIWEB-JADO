eb0aeb1886f9d7f34b887dd75b39d36cfb19892b
Smith <smithoo@nhnnext.org>
2015-04-28 14:07:03 +0900

    기존 코드 라이브러리로 삽입 (setting 제외)

--- webapps/js/jadoJS/JadoLibrary.js
+++ webapps/js/jadoJS/JadoLibrary.js
@@ -1,7 +1,11 @@
 function JADO() {
 	this.util = new Util();
+	this.formManager = new FormManager();
+
 };
 
+// ##### Util #####
+// 프로젝트에서 쓰이는 Util 함수들입니다.
 function Util() {};
 
 Util.prototype.getElement = function(element) {
@@ -15,7 +19,46 @@ Util.prototype.getElementAll = function(element) {
 };
 
 Util.prototype.saveUrl = function() {
-	url = this.getElement('input[name=url]');
+	var url = this.getElement('input[name=url]');
+	if (!url) return;
 	url.value = document.location.href;
+}
+
+// ##### Form Manager #####
+// 아직 정확한 기능이 구현되지 않았습니다.
+// Form validate 역할을 합니다.
+function FormManager() {};
+
+FormManager.prototype.showSellerEnroll = function(element) {
+	var chkBox = document.querySelector(element);
+	if (!chkBox) return;
+	chkBox.addEventListener("click", function() {
+		var target = document.querySelector(".formContainer");
+		if (chkBox.checked) {
+			target.classList.add('scrollSellerEnroll');
+		} else {
+			target.classList.remove('scrollSellerEnroll');
+		}
+	})
+}
+
+FormManager.prototype.whatIsThis = function(element) {
+	var elForm = document.querySelector(element);
+    if (!elForm) return;
+    var elements = {
+        elId: 'userId',
+        elPw1: 'password',
+        elPw2: 'checkPassword',
+        elName: 'name',
+        elPhone: 'phone',
+        elAddress: 'address',
+        elSubmit: 'submit'
+    };
+
+    for (var prop in elements) {
+        if (elements.hasOwnProperty(prop)) {
+            this[prop] = document.querySelector('input[name="' + elements[prop] + '"]');
+        }
+    }
 }
 
--- webapps/js/jadoJS/JadoService.js
+++ webapps/js/jadoJS/JadoService.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function() {
     var jado = new JADO();
     jado.util.saveUrl();
+    jado.formManager.showSellerEnroll("#chkBox");
+    jado.formManager.whatIsThis("#signup_form");
 })
