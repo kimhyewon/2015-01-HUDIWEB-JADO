f3b97e39ec6280057c66b0e7bfb2bb6b5246b411
GraceLilac <lovelove8926@naver.com>
2015-05-09 02:21:19 +0900

    카테고리 클릭시 해당 카테고리로 이

--- src/jado/controller/CategoryController.java
+++ src/jado/controller/CategoryController.java
@@ -1,5 +1,37 @@
 package jado.controller;
 
-public class CategoryController {
+import jado.model.Category;
+import jado.model.Product;
+import jado.service.CategoryService;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value = "/category")
+public class CategoryController {
+	@Autowired
+	private CategoryService categoryService;
+	
+	//블로그 페이지에서 카테고리명 클릭시 category.jsp 보여줌 
+	@RequestMapping(value="/{categoryId}", method = RequestMethod.GET)
+	public String doGet(Model model, @PathVariable("categoryId")String categoryId)
+			throws ServletException, IOException {
+		Category category = categoryService.getCategory(Integer.parseInt(categoryId));
+		List<Product> products = categoryService.getProducts(Integer.parseInt(categoryId));
+		model.addAttribute("category", category);
+		model.addAttribute("products", products);
+		
+		return "category";
+	}
 }
--- src/jado/service/CategoryService.java
+++ src/jado/service/CategoryService.java
@@ -1,5 +1,27 @@
 package jado.service;
 
-public class CategoryService {
+import java.util.List;
 
+import jado.dao.CategoryDao;
+import jado.dao.ProductDao;
+import jado.model.Article;
+import jado.model.Category;
+import jado.model.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+	@Autowired private CategoryDao categoryDao;
+	@Autowired private ProductDao productDao;
+	
+	public Category getCategory(int categoryId) {
+		return categoryDao.selectByPk(categoryId);
+	}
+	
+	public List<Product> getProducts(int categoryId) {
+		return productDao.selectAllByCateGoryId(categoryId);
+	}
+	
 }
