77c070591778aa3cdce5b2f90a02f33bc80d34c8
erin <erin314@naver.com>
2015-05-12 22:14:30 +0900

    오류 고침

--- src/jado/controller/MailAuthController.java
+++ src/jado/controller/MailAuthController.java
@@ -1,5 +1,6 @@
 package jado.controller;
 
+import jado.model.Notice;
 import jado.service.MailAuthService;
 
 import java.util.HashMap;
@@ -11,35 +12,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import core.mail.template.MailTemplateStorage.Type;
+import core.util.ModelAndViewUtils;
 
 @Controller
-@RequestMapping(value ="/mailAuth")
+@RequestMapping(value = "/mailAuth")
 public class MailAuthController {
 
 	@Autowired
 	private MailAuthService mailAuthService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String InvalidAccess(Model model) {
-		model.addAttribute("title", "Invalid  access");
-		model.addAttribute("message", "당신은 접근 권한이 없습니다.");
-		return "notice";
+	public ModelAndView InvalidAccess(Model model) {
+		return ModelAndViewUtils.renderToNotice(new Notice("Invalid  access", "당신은 접근 권한이 없습니다."));
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String processMailAuth(@RequestParam("requestemail") String userEmail, @RequestParam("requestuuid") String uuid, Model model) {
+	public ModelAndView processMailAuth(@RequestParam("requestemail") String userEmail, @RequestParam("requestuuid") String uuid) {
 		if (mailAuthService.isAlreadyVerified(userEmail)) {
-			model.addAttribute("title", "Already Success");
-			model.addAttribute("message", "당신은 이미 인증된 회원 입니다.");
-			return "notice";
+			return ModelAndViewUtils.renderToNotice(new Notice("Already Success", "당신은 이미 인증된 회원 입니다."));
 		}
-		
+
 		if (!mailAuthService.verify(userEmail, uuid)) {
-			model.addAttribute("title", "E-mail authentication failed");
-			model.addAttribute("message", "이메일 인증에 실폐하셨습니다. 다시 시도해 주시기 바랍니다.");
-			return "notice";
+			return ModelAndViewUtils.renderToNotice(new Notice("E-mail authentication failed", "이메일 인증에 실폐하셨습니다. 다시 시도해 주시기 바랍니다."));
 		}
 
 		mailAuthService.updateTypeOfUserRole(userEmail);
@@ -47,8 +44,6 @@ public class MailAuthController {
 		mailParameterMap.put("mailRecipient", userEmail);
 		mailAuthService.send(mailParameterMap, Type.JOIN_WELCOME);
 
-		model.addAttribute("title", "Success");
-		model.addAttribute("message", "이메일 인증에 성공하셨습니다. 감사합니다.");
-		return "notice";
+		return ModelAndViewUtils.renderToNotice(new Notice("Success", "이메일 인증에 성공하셨습니다. 감사합니다."));
 	}
 }
