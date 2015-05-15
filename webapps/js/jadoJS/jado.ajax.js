b1b7c70be23dfd3ee1a028ed8a85a8f70c2ad17c
GraceLilac <lovelove8926@naver.com>
2015-05-15 23:12:52 +0900

    product comment delete ajax

--- webapps/js/jadoJS/jado.ajax.js
+++ webapps/js/jadoJS/jado.ajax.js
@@ -73,3 +73,28 @@ function writeComments(e) {
 	 request.send(params);
 }
 
+//product 댓글 삭제 
+var formList = document.querySelectorAll('#comments input[type=image]');
+for ( var j=0 ; j < formList.length ; j++) {
+	formList[j].addEventListener('click', deleteComments, false);
+}
+
+function deleteComments(e) {
+	 e.preventDefault();
+	 var commentForm = e.currentTarget.form;
+	 var url = "/category/product/answer/delete";
+	 var params = "&shopUrl=" + commentForm[0].value + "&categoryId=" + commentForm[1].value + "&productId=" + commentForm[2].value +
+	 "&userId=" + commentForm[3].value + "&commentTime=" + commentForm[4].value;
+	 
+	 var request = new XMLHttpRequest();
+	 request.open("POST", url, true);
+	 request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	 
+	 request.onreadystatechange = function() {
+		 if(request.readyState == 4 && request.status == 200) {
+			 location.reload(true);
+		 }
+	 }
+	 request.send(params);
+}
+
