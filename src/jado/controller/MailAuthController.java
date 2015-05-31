aaaf3335a10d390efab0b5bfaa2d573bcc61caa2
ho <hataeho1@naver.com>
2015-06-01 04:52:06 +0900

    remove unused logger 2

--- src/jado/controller/MailAuthController.java
+++ src/jado/controller/MailAuthController.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,7 +30,6 @@ import core.util.ModelAndViewUtils;
 @Controller
 @RequestMapping(value = "/mailAuth")
 public class MailAuthController {
-	private static final Logger LOG = LoggerFactory.getLogger(MailAuthController.class);
 	@Autowired
 	private MailAuthService mailAuthService;
 	@Autowired
