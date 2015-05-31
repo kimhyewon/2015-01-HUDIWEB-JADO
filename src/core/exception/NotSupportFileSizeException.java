e4a3a5f58fa534469c12fc8eebf779bdc2e9af80
ho <hataeho1@naver.com>
2015-06-01 04:53:58 +0900

    minor bug fix

--- src/core/exception/NotSupportFileSizeException.java
+++ src/core/exception/NotSupportFileSizeException.java
@@ -1,6 +1,6 @@
 package core.exception;
 
-public class NotSupportFileSizeException extends ForignKeyException {
+public class NotSupportFileSizeException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public NotSupportFileSizeException(String message) {
--- src/core/exception/NotSupportFileTypeException.java
+++ src/core/exception/NotSupportFileTypeException.java
@@ -1,6 +1,6 @@
 package core.exception;
 
-public class NotSupportFileTypeException extends ForignKeyException {
+public class NotSupportFileTypeException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public NotSupportFileTypeException(String message) {
